6e7d84727404fac24e14db99a7244c5e263de0b5
yousef <47471228+yousefdev20@users.noreply.github.com>
2021-11-07 11:39:01 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -20,7 +20,7 @@ define('LARAVEL_START', microtime(true));
 | loading any of our classes later on. It feels great to relax.
 |
 */
-
+# label maintainer = yousef
 require __DIR__.'/../vendor/autoload.php';
 
 /*
